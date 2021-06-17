408b37f9541d7f2a64c1193d605da91eb88fa3cf
Dmitry Baklanov <n79613928275@gmail.com>
2021-06-17 18:32:24 +0400

    Upload image route protection start point

--- pages/api/image.js
+++ pages/api/image.js
@@ -1,82 +1,51 @@
+import nextConnect from 'next-connect'
 import cookie from 'cookie'
-import { API_URL } from '@/config/index'
-import axios from 'axios'
-import fs from 'fs'
+import multer from 'multer'
 
-export default async (req, res) => {
-  console.log(req.files)
-  if (req.method === 'POST') {
-    if (!req.headers.cookie) {
-      res.status(403).json({ message: 'Требуется авторизация' })
-      return
-    }
-    const { token } = cookie.parse(req.headers.cookie)
-    const config = {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }
-    const apiRes = await axios.get(`${API_URL}auth/me`, config)
+const imageRoute = nextConnect({
+  onError(error, req, res) {
+    res.status(501).json({ error: `Что-то пошло не так ${error.message}` })
+  },
+  onNoMatch(req, res) {
+    res.status(405).json({ error: `Метод ${req.method} запрещен` })
+  },
+  //attachParams: true,
+})
 
-    if (apiRes) {
-      if (apiRes.data.data.role !== 'Admin') {
-        res.status(403).json({ message: 'Доступ запрещен' })
-        return
-      } else {
-        if (!req.files) {
-          res.status(400).json({ message: 'Добавь файл с изображением' })
-          return
-        }
-        const folder = req.body.folder || 'img'
-        const file = req.files.file
-        if (!file.mimetype.startsWith('image')) {
-          res.status(400).json({ message: 'Добавь файл с изображением' })
-          return
-        }
-        if (file.size > 1000000) {
-          res
-            .status(400)
-            .json({ message: 'Размер файла изображения не более 1000000' })
-          return
-        }
-        const fullpath = `/images/${folder}/${file.name}`
-
-        if (fs.existsSync(`.${fullpath}`)) {
-          fs.unlink(`.${fullpath}`, (err) => {
-            if (err) throw err
-            file.mv(`.${fullpath}`, (err) => {
-              if (err) {
-                console.error(err)
-                res.status(500).json({ message: 'Проблема при загрузке файла' })
-                return
-              }
-
-              res.status(200).json({
-                success: true,
-                data: fullpath,
-              })
-            })
-          })
-        } else {
-          file.mv(`.${fullpath}`, (err) => {
-            if (err) {
-              console.error(err)
-              res.status(500).json({ message: 'Проблема при загрузке файла' })
-              return
-            }
-
-            res.status(200).json({
-              success: true,
-              data: fullpath,
-            })
-          })
-        }
-      }
-    } else {
-      res.status(403).json({ message: 'Требуется авторизация' })
-    }
-  } else {
-    res.setHeader('Allow', ['POST'])
-    res.status(405).json({ message: `Method ${req.method} not allowed` })
+const upload = multer({
+  limits: { fileSize: 1000000 },
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './public/images/img/')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    },
+  }),
+  fileFilter: function (req, file, cb) {
+    const acceptFile = ['image/jpeg', 'image/png'].includes(file.mimetype)
+    cb(null, acceptFile)
+  },
+})
+// @TODO Доделать проверку на разрешение добавлять фото
+imageRoute.use((req, res, next) => {
+  if (!req.headers.cookie) {
+    res.status(403).json({ message: 'Требуется авторизация' })
+    return
   }
+  const { token } = cookie.parse(req.headers.cookie)
+  // @TODO Вытащить пользователя и проверить его роль
+  console.log(token)
+  next()
+})
+imageRoute.use(upload.single('file'))
+imageRoute.post((req, res) => {
+  res.status(200).json({ data: 'Success', file: req.file.path })
+})
+
+export const config = {
+  api: {
+    bodyParser: false, // Disallow body parsing, consume as stream
+  },
 }
+export default imageRoute
